d8a4c67f9edfb21c7a0b02406987527e7e09736a
Ferry Djaja <ferrygun@yahoo.com>
2020-07-27 19:58:54 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+/*
+var child_process = require('child_process');
+
+child_process.exec('fcopy.bat result.png', function(error, stdout, stderr) {
+    console.log(stdout);
+});
+*/
+"use strict";
+const fs = require('fs');
+const child_process = require('child_process');
+
+var socket = require('socket.io-client')('http://127.0.0.1:5000/');
+socket.on('connect', function(){
+	socket.on('req', function(msg){
+		console.log(msg);
+
+		var base64String = 'data:image/png;base64,' + msg.base64;
+        var base64Image = base64String.split(';base64,').pop();
+
+        var fileName = 'result.png'
+		fs.writeFile(fileName, base64Image, {
+            encoding: 'base64'
+        }, function(err) {
+            console.log('File created: result.png');
+
+			child_process.exec('fcopy.bat result.png', function(error, stdout, stderr) {
+				console.log(stdout);
+			});
+            
+        });
+
+	});
+});
